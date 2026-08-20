7561a48c7231875e4d9e6607eed0ad6743aedf61
a1313855-bit <a1313855@gmail.com>
2026-08-20 16:38:03 +0800

    2026/08/20

---
+++ day65/SpringBootBasicApi0820/src/main/java/com/example/demo/SpringBootBasicApi0820Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicApi0820Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicApi0820Application.class, args);
+	}
+
+}
---
+++ day65/SpringBootBasicApi0820/src/main/java/com/example/demo/controller/BasicApi.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Info;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class BasicApi {
+	@GetMapping("/first")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("Hello World");
+	}
+	
+	@PostMapping
+    public ResponseEntity<String> nameCity(@ModelAttribute Info data ){
+    	String msg="Name:"+data.getName()+" City:"+data.getCity();
+    	return ResponseEntity.ok(msg);
+    }
+}
